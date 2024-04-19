e91186f684ca5099c369633f57c48bcbd9c69b7f
Vikash <rautelavikash27@gmail.com>
2024-04-19 23:19:34 +0530

    improved

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,18 +1,27 @@
 import React from "react";
+import DefaultImage from "../Images/News1.jpg"; // Fallback image
 
-function NewsItem(props) {
-  let { desc, title, imageURL, newsUrl, sourceName } = props;
+function NewsItem({ desc, title, imageURL, newsUrl, sourceName }) {
   return (
-    <div href="#">
+    <div>
       <div className="card my-3">
-        <img src={imageURL} className="card-img-top" alt="..." />
+        <img
+          src={imageURL || DefaultImage}
+          className="card-img-top"
+          alt={title}
+        />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="w-100 fs-6 text-body-secondary text-end">
             - {sourceName}
-          </p> 
+          </p>
           <p className="card-text">{desc}</p>
-          <a href={newsUrl} target="_blank" className="btn btn-primary btn-sm">
+          <a
+            href={newsUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-primary btn-sm"
+          >
             Read More...
           </a>
         </div>
